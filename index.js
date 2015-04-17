30709ef8cc6c0fec0f3d507c4a142219185fc3ef
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-17 10:13:38 -0400

    adding express routing

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Started");
+
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+app.use(bodyParser.json());
+
+var sqlite3	= require('sqlite3').verbose();
+var dbName = './test.db';
+var db = new sqlite3.Database(dbName);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
