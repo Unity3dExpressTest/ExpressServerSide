560ceb7c2ea4bc728de5f0987d5b950f709cd65a
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-17 10:38:45 -0400

    Initing user db

---
+++ dbInit.js
@@ -0,0 +1,38 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./trek.db');
+
+db.serialize(function() {
+  console.log("Database Serialization Initializing...");
+
+  //Setting up info tables
+  setupTable("users", "(id TEXT UNIQUE, name TEXT, x REAL, y REAL)");
+
+  testUsers();
+
+  console.log("Tables initialized!");
+});
+
+function testUsers() {
+	var stmt = db.prepare("INSERT INTO users VALUES (?, ?, ?, ?)");
+	for (var i = 0; i < 2; i++) {
+	  stmt.run("device-" + i, "name " + i, i*1.1, i*2.1);
+	}
+	stmt.finalize();
+
+	db.each("SELECT id, name FROM users", function(err, row) {
+	  console.log(row.id + ": " + row.name + " loc: " + row.x + ", " + row.y);
+	});
+}
+
+function setupTable(table_name, columns) {
+	console.log("Setting up " + table_name + "...");
+
+	db.run("DROP TABLE IF EXISTS " + table_name);
+	db.run("CREATE TABLE " + table_name + " " + columns);
+
+	console.log("Table " + table_name + " initialized!");
+}
+
+db.close(function() {
+      console.log("Database Closed");
+});
--- index.js
+++ index.js
@@ -13,6 +13,36 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/users', function(req, res) {
+	console(req.body);
+	var json;
+	db.all("SELECT location_ id, zipcode, image FROM locs", function(err, rows) {
+		console.log(rows);
+		json = JSON.stringify(rows);
+		console.log(json);
+
+		res.type('text/plain');
+  		res.send(json);
+	});
+});
+
+//Adds user if user doesn't exist. Updates user if user exists
+app.put('/user', function(req, res){
+	console.log(req.body);
+
+	var stmt = db.prepare("INSERT OR REPLACE INTO users (id,name,x,y) VALUES (?, ?, ?, ?)");
+	stmt.run(req.body.id, req.body.name, req.body.x, req.body.y);
+	stmt.finalize();
+
+	console.log("Added User");
+	
+	db.each("SELECT id, username FROM users", function(err, row) {
+	  console.log(row.id + ": " + row.name + " loc: " + row.x + ", " + row.y);
+	});
+
+	res.send(req.body);
+});
+
 var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
